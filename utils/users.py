6f57e1694b4c9da3ad11ad8888fba691bbccec6f
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-10-19 23:01:15 +0300

    update email

--- utils/users.py
+++ utils/users.py
@@ -15,8 +15,9 @@ def get_users(vc_type):
     sleep(random.uniform(0, 1.5))
     if vc_type == 'Inviting':
         vc_type = ('Inviting', 'Buisness')
-    t = sys.argv[2]
-    logging.warning(f'sys.argv[2]: {t}')
+    t = sys.argv[2].replace(':', ';')
+    e = t.replace(';', ':')
+    logging.warning(f'args: {str(e)}')
     users = s.get(sys.argv[2])
     users = [user for user in json.loads(users.text) if user["vc_type"] in vc_type]
     return users  # test users [{'id': '1', 'vc_status': '2', 'vc_city': 'Schengenvisa', 'vc_type': 'Inviting', 'vc_mail': 'sash.kardash@gmail.com', 'vc_inviting': 'Grigory Fray', 'vc_passport': 'MP8338818', 'vc_birth': '1965-10-16', 'vc_passport_from': '2021-07-20', 'vc_passport_to': '2031-07-20', 'vc_passport_by': 'MIA', 'vc_name': 'Valery', 'vc_surname': 'Vetlitcky', 'vc_phone': '+375256062209', 'vc_date_first_travel': '2022-08-15', 'vc_date_from': '2022-07-24', 'vc_date_to': '', 'vc_with': '0', 'vc_comment': '', 'vc_first': '1', 'vc_today': '1', 'vc_before_visit': '0'}, {'id': '3', 'vc_status': '2', 'vc_city': 'Schengenvisa', 'vc_type': 'Inviting', 'vc_mail': 'sash.kardash@gmail.com', 'vc_inviting': 'Grigory Fray', 'vc_passport': 'MP8338818', 'vc_birth': '1965-10-16', 'vc_passport_from': '2021-07-20', 'vc_passport_to': '2031-07-20', 'vc_passport_by': 'MIA', 'vc_name': 'Valery', 'vc_surname': 'Vetlitcky', 'vc_phone': '+375256062209', 'vc_date_first_travel': '2022-08-15', 'vc_date_from': '2022-07-24', 'vc_date_to': '', 'vc_with': '0', 'vc_comment': '', 'vc_first': '1', 'vc_today': '1', 'vc_before_visit': '0'}]
