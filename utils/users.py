cfc86c5c4545cc9bd401b7a895bcf21592608e59
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-10-19 23:30:46 +0300

    update email

--- utils/users.py
+++ utils/users.py
@@ -8,14 +8,14 @@ from time import sleep
 import requests
 
 s = requests.Session()
-s.auth = ('rest_user', sys.argv[1])
+s.auth = ('rest_user', sys.argv[1].replace('"', ''))
 
 
 def get_users(vc_type):
     sleep(random.uniform(0, 1.5))
     if vc_type == 'Inviting':
         vc_type = ('Inviting', 'Buisness')
-    users = s.get(sys.argv[2])
+    users = s.get(sys.argv[2].replace('"', ''))
     users = [user for user in json.loads(users.text) if user["vc_type"] in vc_type]
     return users
 
